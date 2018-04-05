038db37c42215c7ae24866fa33fcaa6566a04d31
arniebradfo <arniebradfo@gmail.com>
2018-04-04 23:14:16 -0600

    form open and close

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding, ViewChild, ElementRef } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
 // search form that routes to a post-list filtered by the search string
@@ -12,6 +12,15 @@ export class SearchComponent implements OnInit {
 
 	search: string;
 
+	@HostBinding('class.is-open')
+	isOpen: boolean = false;
+
+	@ViewChild('searchInput')
+	searchInput: ElementRef;
+
+	@ViewChild('searchSubmit')
+	searchSubmit: ElementRef;
+
 	constructor(
 		private router: Router,
 		private activatedRoute: ActivatedRoute
@@ -24,6 +33,33 @@ export class SearchComponent implements OnInit {
 			queryParams: { s: this.search },
 			relativeTo: this.activatedRoute.root
 		});
+		// window.setTimeout(() => this.close(), 0)
+		this.close();
+	}
+
+	hasFocus(): boolean {
+		const focused = document.activeElement;
+		return this.searchInput.nativeElement.nativeElement === focused || this.searchSubmit.nativeElement === focused;
+	}
+
+	open() {
+		this.isOpen = true;
+		window.setTimeout(() => this.searchInput.nativeElement.focus(), 0)
+	}
+	close() {
+		this.isOpen = false;
+		window.setTimeout(() => this.searchInput.nativeElement.blur(), 0)
+		this.search = '';
+	}
+	toggleOpen() {
+		return this.isOpen ? this.close() : this.open();
+	}
+
+	maybeClose() {
+		window.setTimeout(() => {
+			if (!this.hasFocus())
+				this.close();
+		}, 0)
 	}
 
 }
