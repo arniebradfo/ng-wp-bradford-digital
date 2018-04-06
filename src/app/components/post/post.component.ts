2592dc258f0c1244ccc56fe89e2329908621e10d
arniebradfo <arniebradfo@gmail.com>
2018-04-05 18:53:47 -0600

    full post header layout css

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,30 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
 import { IWpPage, IWpPost } from '../../interfaces/wp-rest-types';
 import { ViewModelService } from '../../services/view-model.service';
 
 @Component({
-  selector: 'ngwp-post',
-  templateUrl: './post.component.html',
-  styleUrls: ['./post.component.less']
+	selector: 'ngwp-post',
+	templateUrl: './post.component.html',
+	styleUrls: ['./post.component.less']
 })
 export class PostComponent implements OnInit {
 
-  @Input() post: IWpPage | IWpPost;
-  @Input() showFull: boolean = false;
+	@Input() post: IWpPage | IWpPost;
 
-  private showPasswordForm: boolean = false;
-  private password: string;
+	@HostBinding('class.show-full')
+	@Input() showFull: boolean = false;
 
-  constructor(
-    private viewModelService: ViewModelService
-  ) { }
 
-  ngOnInit() {
-    this.showPasswordForm = !!this.post.content.protected;
-  }
+	private showPasswordForm: boolean = false;
+	private password: string;
 
-  public onSubmitPassword(): void {
-    this.viewModelService.getPasswordProtected(this.post.id, this.password);
-  }
+	constructor(
+		private viewModelService: ViewModelService
+	) { }
+
+	ngOnInit() {
+		this.showPasswordForm = !!this.post.content.protected;
+	}
+
+	public onSubmitPassword(): void {
+		this.viewModelService.getPasswordProtected(this.post.id, this.password);
+	}
 
 }
